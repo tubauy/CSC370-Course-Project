8dd7bcc06d1ad528431b899848664dddd60e36a0
Tyler Kettner <tylerkettner@uvic.ca>
2026-07-17 14:20:22 -0700

    Added tables for Motherboards, RAM, and CPU

---
+++ setup.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Motherboards(
+    `name` varchar(256) PRIMARY KEY,
+    `socket` varchar(64),
+    `ddr_version` int;
+);
+
+CREATE TABLE RAM(
+    `name` varchar(256) PRIMARY KEY,
+    `capacity_MB` int,
+    `ddr_version` int
+);
+
+CREATE TABLE CPU(
+    `name` varchar(256) PRIMARY KEY,
+    `socket` varchar(64),
+    `clock_speed_Hz` int,
+    `ddr_version` int
+);
