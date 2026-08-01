1ac303abb56ab4c311d26ad9117fb9af73472eff
Tyler Kettner <tylerkettner@uvic.ca>
2026-08-01 15:20:27 -0700

    Added Manufacturers, Components, and PSU tables, and updated Motherboards to have more attributes. All component tables  attributes have been moved to the components metadata table

--- setup.sql
+++ setup.sql
@@ -1,6 +1,29 @@
+CREATE TABLE Manufacturers(
+    `name` VARCHAR(255) PRIMARY KEY,
+    `country` VARCHAR(255),
+    `address` VARCHAR(255)
+)
+
+
+-- Component table holds meta data for all components --
+CREATE TABLE Components(
+    `component_id` int AUTO_INCREMENT PRIMARY KEY,
+    `part_type` ENUM(
+        'Motherboard', 
+        'RAM', 
+        'CPU', 
+        'Storage', 
+        'GPU', 
+        'PSU'
+    ),
+    `name` VARCHAR(255),
+    `manufacturer` VARCHAR(255),
+    `pirce` FLOAT,
+    `release_date` DATE
+)
+
 CREATE TABLE Motherboards(
     `component_id` int PRIMARY KEY,
-    `name` varchar(128),
     `form_factor` varchar(32),
     `socket_type` varchar(16),
     -- RAM --
@@ -21,12 +44,19 @@ CREATE TABLE Motherboards(
     `SATA_3_ports` int,
     `U_2_ports` int
     -- Network --
-    -- Headers --
+    `ethernet_speed` ENUM(
+        '10 Gb/s',
+        '5 Gb/s',
+        '2.5 Gb/s',
+        '1 Gb/s',
+        '100 Mb/s',
+        'N/A'
+    )
+    `WiFi` VARCHAR(32)
 );
 
 CREATE TABLE RAM(
     `component_id` int PRIMARY KEY,
-    `name` varchar(128),
     `capacity_MB` int,
     `ddr_version` int,
     `speed_MHz` int
@@ -34,7 +64,6 @@ CREATE TABLE RAM(
 
 CREATE TABLE CPU(
     `component_id` int PRIMARY KEY,
-    `name` varchar(128),
     `socket_type` varchar(16),
     `cores` int,
     `clock_speed_MHz` int,
@@ -55,10 +84,23 @@ CREATE TABLE Storage(
 
 CREATE TABLE GPU(
     `component_id` int PRIMARY KEY,
-    `name` varchar(128),
     `clock_speed_MHz` int,
     `vram_GB` int,
     `vram_type` varchar(16),
     `PCIe_gen` float,
     `PCIe_lanes` int
 );
+
+CREATE TABLE PSU(
+    'component_id' int,
+    `wattage` int,
+    `form_factor` VARCHAR(16),
+    `efficiency_rating` VARCHAR(16),
+    -- Connectors --
+    `atx_24_pin` int,
+    `eps_8_pin` int,
+    `pcie_12vhpwr` int,
+    `pcie_6_plus_2` int,
+    `sata` int,
+    `molex_4_pin` int
+)
