517f105d11abaed3a21393f162523ee194ef0993
Sebastian Dawson <sebastaind@uvic.ca>
2026-08-02 00:41:59 -0700

    changed CurrentBuild dict values to string concatenation to avoid newline char errors

--- current_build.py
+++ current_build.py
@@ -8,74 +8,74 @@ class CurrentBuild:
     #TODO keep empty strings where no compatability rules exist?
     #TODO parse mirrored tuples so they don't have to be included
     join_conditions_dict = {
-        ("CPU","Motherboards"): 
-        """SELECT `component_id`
-        FROM `Motherboards`
-        WHERE `Motherboards`.`socket_type` = {cpu_id_socket_type}""",
-        ("Motherboards","CPU"): 
-        """SELECT `component_id`
-        FROM `CPU`
-        WHERE `CPU`.`socket_type` = {motherboard_id_socket_type}""",
-        ("CPU","GPU"): 
-        """SELECT `component_id`
-        FROM `GPU`""",
-        ("GPU","CPU"): 
-        """SELECT `component_id`
-        FROM `CPU`""",
-        ("CPU","RAM"): 
-        """SELECT `component_id`
-        FROM `RAM`
-        WHERE `RAM`.`ddr_version` = {cpu_id_ddr_version}
-        AND `RAM`.`capacity_MB` <= {cpu_id_max_ram_capacity}""",
-        ("RAM","CPU"): 
-        """SELECT `component_id`
-        FROM `CPU`
-        WHERE `CPU`.`ddr_version` = {ram_id_ddr_version}
-        AND `CPU`.`max_ram_capacity_MB` >= {cpu_id_max_ram_capacity}""",
-        ("CPU","Storage"): 
-        """SELECT `component_id`
-        FROM `Storage`""",
-        ("Storage","CPU"): 
-        """SELECT `component_id`
-        FROM `CPU`""",
-        ("Motherboards","GPU"): 
-        """SELECT `component_id`
-        FROM `GPU`""",
-        ("GPU","Motherboards"): 
-        """SELECT `component_id`
-        FROM `Motherboards`""",
-        ("Motherboards","RAM"): 
-        """SELECT `component_id`
-        FROM `RAM`
-        WHERE `RAM`.`ddr_version` = {motherboard_id_ddr_version}""",
-        ("RAM","Motherboards"): 
-        """SELECT `component_id`
-        FROM `Motherboards`
-        WHERE `Motherboards`.`ddr_version` = {ram_id_ddr_version}""",
-        ("Motherboards","Storage"):
-        """SELECT `component_id`
-        FROM `Storage`""",
-        ("Storage","Motherboards"):
-        """SELECT `component_id`
-        FROM `Motherboards`""",
-        ("GPU","RAM"):
-        """SELECT `component_id`
-        FROM `RAM`""",
-        ("RAM","GPU"):
-        """SELECT `component_id`
-        FROM `GPU`""",
-        ("GPU","Storage"):
-        """SELECT `component_id`
-        FROM `Storage`""",
-        ("Storage","GPU"):
-        """SELECT `component_id`
-        FROM `GPU`""",
-        ("RAM","Storage"):
-        """SELECT `component_id`
-        FROM `Storage`""",
-        ("Storage","RAM"):
-        """SELECT `component_id`
-        FROM `RAM`"""
+        ("CPU","Motherboards"): (
+        "SELECT `component_id` "
+        "FROM `Motherboards` "
+        "WHERE `Motherboards`.`socket_type` = {cpu_id_socket_type}"),
+        ("Motherboards","CPU"): (
+        "SELECT `component_id` "
+        "FROM `CPU` "
+        "WHERE `CPU`.`socket_type` = {motherboard_id_socket_type}"),
+        ("CPU","GPU"): (
+        "SELECT `component_id` "
+        "FROM `GPU` "),
+        ("GPU","CPU"): (
+        "SELECT `component_id` "
+        "FROM `CPU`"),
+        ("CPU","RAM"): (
+        "SELECT `component_id` "
+        "FROM `RAM` "
+        "WHERE `RAM`.`ddr_version` = {cpu_id_ddr_version} "
+        "AND `RAM`.`capacity_MB` <= {cpu_id_max_ram_capacity}"),
+        ("RAM","CPU"): (
+        "SELECT `component_id` "
+        "FROM `CPU` "
+        "WHERE `CPU`.`ddr_version` = {ram_id_ddr_version} "
+        "AND `CPU`.`max_ram_capacity_MB` >= {cpu_id_max_ram_capacity} "),
+        ("CPU","Storage"): (
+        "SELECT `component_id` "
+        "FROM `Storage` "),
+        ("Storage","CPU"): (
+        "SELECT `component_id` "
+        "FROM `CPU` "),
+        ("Motherboards","GPU"): (
+        "SELECT `component_id` "
+        "FROM `GPU`"),
+        ("GPU","Motherboards"): (
+        "SELECT `component_id` "
+        "FROM `Motherboards`"),
+        ("Motherboards","RAM"): (
+        "SELECT `component_id` "
+        "FROM `RAM` "
+        "WHERE `RAM`.`ddr_version` = {motherboard_id_ddr_version}"),
+        ("RAM","Motherboards"): (
+        "SELECT `component_id` "
+        "FROM `Motherboards` "
+        "WHERE `Motherboards`.`ddr_version` = {ram_id_ddr_version} "),
+        ("Motherboards","Storage"): (
+        "SELECT `component_id` "
+        "FROM `Storage`"),
+        ("Storage","Motherboards"): (
+        "SELECT `component_id` "
+        "FROM `Motherboards`"),
+        ("GPU","RAM"): (
+        "SELECT `component_id` "
+        "FROM `RAM`"),
+        ("RAM","GPU"): (
+        "SELECT `component_id` "
+        "FROM `GPU`"),
+        ("GPU","Storage"): (
+        "SELECT `component_id` "
+        "FROM `Storage`"),
+        ("Storage","GPU"): (
+        "SELECT `component_id` "
+        "FROM `GPU`"),
+        ("RAM","Storage"): (
+        "SELECT `component_id` "
+        "FROM `Storage`"),
+        ("Storage","RAM"): (
+        "SELECT `component_id` "
+        "FROM `RAM`")
     }
 
     def __init__(self, DBconnection):
@@ -91,7 +91,7 @@ class CurrentBuild:
         }
 
         self.connection = DBconnection
-        self.not_yet_picked = []
+        self.not_yet_picked = ["Motherboard", "CPU", "GPU", "RAM", "Storage"]
 
 
     def test_output(self):
@@ -121,6 +121,7 @@ class CurrentBuild:
             #run query
             #loop through already picked items and run query for each, based on appropiate condition
             #return list of tuples
+            query = ""
             with self.connection.cursor() as cursor:
                 pass
 
