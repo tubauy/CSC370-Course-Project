2b15066e0461664ef721af08f99c8f119ff492ff
Tyler Kettner <tylerkettner@uvic.ca>
2026-08-02 12:10:27 -0700

    Added scheta for Users and Configurations. Every attribute in configurations (except for configuration_id) is a reference to other tables.

--- setup.sql
+++ setup.sql
@@ -1,3 +1,37 @@
+CREATE TABLE Users(
+    `user_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255),
+    `email` VARCHAR(255),
+    `date_created` DATETIME
+)
+
+CREATE TABLE Configurations(
+    `configuration_id` INT AUTO_INCREMENT PRIMARY KEY,
+    -- Owner of configuration -- 
+    `user_id` INT REFERENCES `Users`(`user_id`)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    -- Component IDs --
+    `motherboard_id` INT REFERENCES `Motherboards`(`component_id`)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    `ram_id` INT REFERENCES `RAM`(`component_id`)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    `cpu_id` INT REFERENCES `CPUs`(`component_id`)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    `storage_id` INT REFERENCES `Storage`(`component_id`)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    `gpu_id` INT REFERENCES `GPUs`(`component_id`)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    `psu_id` INT REFERENCES `PSUs`(`component_id`)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+)
+
 CREATE TABLE Manufacturers(
     `name` VARCHAR(255) PRIMARY KEY,
     `country` VARCHAR(255),
@@ -6,7 +40,7 @@ CREATE TABLE Manufacturers(
 
 -- Component table holds meta data for all components --
 CREATE TABLE Components(
-    `component_id` int AUTO_INCREMENT PRIMARY KEY,
+    `component_id` INT AUTO_INCREMENT PRIMARY KEY,
     `part_type` ENUM(
         'Motherboard', 
         'RAM', 
@@ -17,31 +51,31 @@ CREATE TABLE Components(
     ),
     `name` VARCHAR(255),
     `manufacturer` VARCHAR(255),
-    `price` FLOAT,
+    `pirce` FLOAT,
     `release_date` DATE
 )
 
 CREATE TABLE Motherboards(
-    `component_id` int PRIMARY KEY,
-    `form_factor` varchar(32),
-    `socket_type` varchar(16),
+    `component_id` INT PRIMARY KEY,
+    `form_factor` VARCHAR(32),
+    `socket_type` VARCHAR(16),
     -- RAM --
-    `ddr_version` int,
-    `ram_slots` int,
+    `ddr_version` INT,
+    `ram_slots` INT,
     -- PCIe --
-    `PCIe_1_gen` float,
-    `PCIe_1_slots` int,
-    `PCIe_4_gen` float,
-    `PCIe_4_slots` int,
-    `PCIe_8_gen` float,
-    `PCIe_8_slots` int,
-    `PCIe_16_gen` float,
-    `PCIe_16_slots` int,
+    `PCIe_1_gen` FLOAT,
+    `PCIe_1_slots` INT,
+    `PCIe_4_gen` FLOAT,
+    `PCIe_4_slots` INT,
+    `PCIe_8_gen` FLOAT,
+    `PCIe_8_slots` INT,
+    `PCIe_16_gen` FLOAT,
+    `PCIe_16_slots` INT,
     -- Storage --
-    `M_2_slots` int,
-    `SATA_6_ports` int,
-    `SATA_3_ports` int,
-    `U_2_ports` int
+    `M_2_slots` INT,
+    `SATA_6_ports` INT,
+    `SATA_3_ports` INT,
+    `U_2_ports` INT
     -- Network --
     `ethernet_speed` ENUM(
         '10 Gb/s',
@@ -55,51 +89,51 @@ CREATE TABLE Motherboards(
 );
 
 CREATE TABLE RAM(
-    `component_id` int PRIMARY KEY,
-    `capacity_MB` int,
-    `ddr_version` int,
-    `speed_MHz` int
+    `component_id` INT PRIMARY KEY,
+    `capacity_MB` INT,
+    `ddr_version` INT,
+    `speed_MHz` INT
 );
 
-CREATE TABLE CPU(
-    `component_id` int PRIMARY KEY,
-    `socket_type` varchar(16),
-    `cores` int,
-    `clock_speed_MHz` int,
+CREATE TABLE CPUs(
+    `component_id` INT PRIMARY KEY,
+    `socket_type` VARCHAR(16),
+    `cores` INT,
+    `clock_speed_MHz` INT,
     -- RAM Compatibility --
-    `max_ram_capacity_MB` int,
-    `ddr_version` int
+    `max_ram_capacity_MB` INT,
+    `ddr_version` INT
 );
 
 CREATE TABLE Storage(
-    `component_id` int PRIMARY KEY,
-    `capacity_GB` int,
-    `storage_type` varchar(16),
-    `form_factor` varchar(16),
-    `interface` varchar(32),
-    `cache_MB` int,
-    `nvme` boolean
+    `component_id` INT PRIMARY KEY,
+    `capacity_GB` INT,
+    `storage_type` VARCHAR(16),
+    `form_factor` VARCHAR(16),
+    `INTerface` VARCHAR(32),
+    `cache_MB` INT,
+    `nvme` BOOLEAN
 );
 
-CREATE TABLE GPU(
-    `component_id` int PRIMARY KEY,
-    `clock_speed_MHz` int,
-    `vram_GB` int,
-    `vram_type` varchar(16),
-    `PCIe_gen` float,
-    `PCIe_lanes` int
+CREATE TABLE GPUs(
+    `component_id` INT PRIMARY KEY,
+    `clock_speed_MHz` INT,
+    `vram_GB` INT,
+    `vram_type` VARCHAR(16),
+    `PCIe_gen` FLOAT,
+    `PCIe_lanes` INT
 );
 
-CREATE TABLE PSU(
-    'component_id' int,
-    `wattage` int,
+CREATE TABLE PSUs(
+    'component_id' INT,
+    `wattage` INT,
     `form_factor` VARCHAR(16),
     `efficiency_rating` VARCHAR(16),
     -- Connectors --
-    `atx_24_pin` int,
-    `eps_8_pin` int,
-    `pcie_12vhpwr` int,
-    `pcie_6_plus_2` int,
-    `sata` int,
-    `molex_4_pin` int
+    `atx_24_pin` INT,
+    `eps_8_pin` INT,
+    `pcie_12vhpwr` INT,
+    `pcie_6_plus_2` INT,
+    `sata` INT,
+    `molex_4_pin` INT
 )
