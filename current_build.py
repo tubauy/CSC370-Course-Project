6e88ae89e306d1492485d1865144c71af8f6694d
Sebastian Dawson <sebastaind@uvic.ca>
2026-08-02 00:27:11 -0700

    Changed dict of pairings to subqueries instead of joins (in CurrentBuild class)

--- current_build.py
+++ current_build.py
@@ -2,26 +2,86 @@
 #passed a mysqlconnector connection object to use
 import mysql.connector
 class CurrentBuild:
-    #class variable of join condtions
-    #be careful of mirrored tuples, must be cleaned up before using dict
+    #class variable of WHERE conditions
+    #first item in tuple is item already picked, second is relation to be searched
     #TODO fill in missing conditions if applicable
     #TODO keep empty strings where no compatability rules exist?
+    #TODO parse mirrored tuples so they don't have to be included
     join_conditions_dict = {
-        ("CPU","Motherboards"): "ON (`CPU`.`socket_type` = `Motherboards`.`Socket_type)",
-        ("CPU","GPU"):"",
-        ("CPU","RAM"): "ON (`CPU`.`ddr_version` = `RAM`.`ddr_version` AND `CPU`.`max_ram_capacity_MB` >= `RAM`.`capacity_MB`)",
-        ("CPU","Storage"):"",
-        ("Motherboards","GPU"):"",
-        ("Motherboards","RAM"): "ON (`Motherboards`.`ddr_version` = `RAM`.`ddr_version`)",
-        ("Motherboards","Storage"):"",
-        ("GPU","RAM"):"",
-        ("GPU","Storage"):"",
-        ("RAM","Storage"):""
+        ("CPU","Motherboards"): 
+        """SELECT `component_id`
+        FROM `Motherboards`
+        WHERE `Motherboards`.`socket_type` = {cpu_id_socket_type}""",
+        ("Motherboards","CPU"): 
+        """SELECT `component_id`
+        FROM `CPU`
+        WHERE `CPU`.`socket_type` = {motherboard_id_socket_type}""",
+        ("CPU","GPU"): 
+        """SELECT `component_id`
+        FROM `GPU`""",
+        ("GPU","CPU"): 
+        """SELECT `component_id`
+        FROM `CPU`""",
+        ("CPU","RAM"): 
+        """SELECT `component_id`
+        FROM `RAM`
+        WHERE `RAM`.`ddr_version` = {cpu_id_ddr_version}
+        AND `RAM`.`capacity_MB` <= {cpu_id_max_ram_capacity}""",
+        ("RAM","CPU"): 
+        """SELECT `component_id`
+        FROM `CPU`
+        WHERE `CPU`.`ddr_version` = {ram_id_ddr_version}
+        AND `CPU`.`max_ram_capacity_MB` >= {cpu_id_max_ram_capacity}""",
+        ("CPU","Storage"): 
+        """SELECT `component_id`
+        FROM `Storage`""",
+        ("Storage","CPU"): 
+        """SELECT `component_id`
+        FROM `CPU`""",
+        ("Motherboards","GPU"): 
+        """SELECT `component_id`
+        FROM `GPU`""",
+        ("GPU","Motherboards"): 
+        """SELECT `component_id`
+        FROM `Motherboards`""",
+        ("Motherboards","RAM"): 
+        """SELECT `component_id`
+        FROM `RAM`
+        WHERE `RAM`.`ddr_version` = {motherboard_id_ddr_version}""",
+        ("RAM","Motherboards"): 
+        """SELECT `component_id`
+        FROM `Motherboards`
+        WHERE `Motherboards`.`ddr_version` = {ram_id_ddr_version}""",
+        ("Motherboards","Storage"):
+        """SELECT `component_id`
+        FROM `Storage`""",
+        ("Storage","Motherboards"):
+        """SELECT `component_id`
+        FROM `Motherboards`""",
+        ("GPU","RAM"):
+        """SELECT `component_id`
+        FROM `RAM`""",
+        ("RAM","GPU"):
+        """SELECT `component_id`
+        FROM `GPU`""",
+        ("GPU","Storage"):
+        """SELECT `component_id`
+        FROM `Storage`""",
+        ("Storage","GPU"):
+        """SELECT `component_id`
+        FROM `GPU`""",
+        ("RAM","Storage"):
+        """SELECT `component_id`
+        FROM `Storage`""",
+        ("Storage","RAM"):
+        """SELECT `component_id`
+        FROM `RAM`"""
     }
 
     def __init__(self, DBconnection):
         #for now, passing connection object instead of cursor
         #allow custom set of parts, i.e add psu or multple storage?
+        #values of dict are part id's
         self.picked_items_id = {
             "Motherboard": None,
             "CPU": None,
