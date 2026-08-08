1c1edd8c0bc15aff024da0bf6b2df76fe4d9bf5c
Sebastian Dawson <sebastaind@uvic.ca>
2026-08-08 00:17:47 -0700

    cleaning up comments and deprecated debugging functions

--- current_build.py
+++ current_build.py
@@ -1,10 +1,12 @@
 #class to track current user config
-#passed a mysqlconnector connection object to use
+#passed a mysqlconnector connection object to use when instantiated
 #TODO: make sure inputs are sanitized
 import mysql.connector
 class CurrentBuild:
-    #class variable of WHERE conditions
+    #class variable of ON conditions for JOINS
     #first item in tuple is item already picked, second is relation to be searched
+    #empty strings indicate no compatibility constraints, i.e outputs all parts in second category of key pair
+
     #TODO fill in missing conditions if applicable (MOTHERBOARD, GPU)
     #TODO keep empty strings where no compatability rules exist?
     #TODO account for mirrored tuples (for now hardcoding mirrors)
@@ -35,9 +37,8 @@ class CurrentBuild:
     }
 
     def __init__(self, DBconnection):
-        #for now, passing connection object instead of cursor
-        #allow custom set of parts, i.e add psu or multple storage?
-        #values of dict are part id's
+        #Passed connection object, because connection object (not cursor) needed for transaction functions 
+        #values of dict are component_id of picked parts
         self.picked_items_id = {
             "Motherboards": None,
             "CPU": None,
@@ -46,7 +47,6 @@ class CurrentBuild:
             "Storage": None
         }
 
-        #always stored picked_items in SQL server to avoid data corruption issues?
         self.connection = DBconnection
 
 
@@ -58,15 +58,14 @@ class CurrentBuild:
             return True
 
     def output_compatible(self, type_to_output):
-        #output parts of the chosen type compatible with current build
-        #returns list of tuples
-        #TODO: account for mirrored tuples
+        #output parts in category type_to_output that are compatible with current picks (from picked_items_id)
+        #returns parts as list of tuples in form (id, name)
+
+        #builds large JOIN condition, using ON conditions from class variable dict
+        #subquerys return parts with specific id using pick_items_id, then those subqueries are aliased and added to JOIN chain
         if(type_to_output not in self.picked_items_id or self.already_picked(type_to_output)):
             return []
         else:
-            #run query
-            #loop through already picked items and run query for each, based on appropiate condition
-            #return list of tuples
             start_query = f"SELECT `{type_to_output}`.`component_id`, `{type_to_output}`.`name` FROM `{type_to_output}` "
 
             #get list of items already picked
@@ -74,26 +73,23 @@ class CurrentBuild:
             with self.connection.cursor() as cursor:
                 count = 1
                 for k in current_picked:
-                    #get table of parts with that id, all attributes
+                    #get table of parts with that id, all attributes to allow for JOINs to have any ON conditions
                     start_query += "JOIN "
                     start_query += f"(SELECT * FROM `{k}` WHERE `{k}`.`component_id` = %({k})s) AS p{count} "
-
                     #join this table (holding one specific part) with table of type_to_output
-                    #TODO: allow for insertion of alias into on condition taken from join_conditions_dict (CRITICAL)
                     on_condition = f"{self.join_conditions_dict[(k,type_to_output)]} "
+                    #string replace function replaces part we already have in ON conditions with appropiate alias
                     start_query += on_condition.replace(k, f"p{count}")
                     count += 1
-                #final_query = " ".join(query_commands)
                 cursor.execute(start_query, self.picked_items_id)
+                #using parameters for values of picked_items_id, as those values can be input by user
                 result = list(cursor.fetchall())
                 return result
 
 
     def add_part_test(self,category, partId: int):
-        #add given part id to the current set
-        #check if part of that type already picked
-        #check compatability as well
-        #for now, indicate category as well
+        #add given part id to dict of picked items
+        #takes category of part being added, and part id of the part
         if(not self.already_picked(category)):
             self.picked_items_id[category] = partId
             #TODO: check if that part is in database? Check with configurations table?
@@ -104,11 +100,8 @@ class CurrentBuild:
 
 
     def exit_and_save(self):
-        #save current config to DB Configurations table and exit
-        #destructor method?
-        #add transactions, check if id's valid?
-
-        #testing out cursor.execute parameters and implicit concatenation, as per the mysql-conncector dev guide
+        #save current config to DB Configurations table
+        #using parameterized query for part id's as user can input those
         query = (
             "INSERT INTO `Configurations`(`Motherboard_id`,`CPU_id`,`GPU_id`,`RAM_id`,`Storage_id`) "
             "VALUES (%(Motherboards)s, %(CPU)s, %(GPU)s, %(RAM)s, %(Storage)s)"
