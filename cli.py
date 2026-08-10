8e86bebf62d2ce72a0c57ba64ce1e493bb181fb6
Tung Do <134229448+tungdo8842@users.noreply.github.com>
2026-08-10 16:14:50 -0700

    Add component selection to cli

--- cli.py
+++ cli.py
@@ -3,59 +3,89 @@ from current_build import CurrentBuild
 class Client:
     def __init__(self, connection):
         # selections can be converted to int if needed
-        self.selections = {"1": "Build a PC from scratch", "2": "Edit your configuration"}
+        self.compoment_selections = {"1": "Motherboards", "2": "CPUs", "3": "RAM", "4": "Storage", "5": "GPU", "6": "PSUs"}
+        self.start_selections = {"1": "Build a PC from scratch", "2": "Edit your existing build (currently not working)"}
+        self.component_cache = {}
+        self._max_build_name_length = 255
+
         self.connection = connection
         self.current_build = None
 
     def start(self):
         print("-----------------------------")
         print("What do you want to do today?")
-        for keys in self.selections:
-            print(f"{keys}: {self.selections[keys]}")
+        for key, value in self.start_selections.items():
+            print(f"{key}) {value}")
         print("-----------------------------")
 
         selection = input("Select: ")
-        while selection not in self.selections.keys():
+        while selection not in self.start_selections.keys():
             print("Please select one of the above options")
             selection = input("Select: ")
         
-        print(f"You chose to {self.selections[selection]}")
+        print(f"You chose to {self.start_selections[selection]}")
         if selection == "1":
-            self.current_build = CurrentBuild(self.connection)
+            new_build_name = input("Name your build: ")
+            while len(new_build_name) <= 0 or len(new_build_name) > self._max_build_name_length:
+                print("Name length should be from 1 to 255 characters")
+                new_build_name = input("Name your build: ")
+            self.current_build = CurrentBuild(self.connection, config_name=new_build_name)
+            self.edit_build_loop()
         elif selection == "2":
-            config_name = input("What do you like to name your Build: ")
-            self.current_build = CurrentBuild(self.connection, config_name=config_name)
+            "Build edit not yet implemented"
+            # enter username -> list builds -> pick
+            return
 
-        print(self.current_build.output_compatible("Motherboards"))
+        self.print_build_info()
+
+    def edit_build_loop(self):
+        while True:
+            print("Select component to choose or edit (or -1 to exit)")
+            for key, value in self.compoment_selections.items():
+                print(f"{key}: {value}")
+
+            selection = input("Select: ")
+            while (selection not in self.compoment_selections.keys() and selection != "-1"):
+                print("Please select one of the above options")
+                selection = input("Select: ")
+            if selection == "-1":
+                return
+
+            component_str = self.compoment_selections[selection]
+
+            # store selected component into cache
+            self.component_cache[component_str] = self.current_build.output_compatible(component_str)
+            self.build_component_cache_dict(component_str)
+            # print(self.component_cache[component_str])
+            self.print_component_selection(component_str)
+
+            selection = input("Select: ")
+            while selection not in self.component_cache[component_str].keys():
+                print("Please select one of the above options")
+                selection = input("Select: ")
+
+            self.current_build.add_part_test(component_str, int(selection))
+            print(f"Selected {component_str} with id {int(selection)}")
+            print()
+
+    def print_component_selection(self, component_str):
+        for cid, name in self.component_cache[component_str].items():
+            print(f"{cid}) {name}")
+
+    def build_component_cache_dict(self, component_str):
+        self.component_cache[component_str] = {}
+        cur_output = self.current_build.output_compatible(component_str)
+        for (cid, name) in cur_output:
+            self.component_cache[component_str][str(cid)] = name
+
+    def print_build_info(self):
+        print("-----------------------")
+        print("Final build components:")
+        for key in self.component_cache.keys():
+            cid = self.current_build.picked_items_id[key]
+            name = self.component_cache[key][str(cid)]
+            print(f"{key}: {name}")
+        print("-----------------------")
 
 
-# def get_priority():
-#     """
-#     priority message, will run in build from scratch,
-#     or configurations with more than 1 item missing
-#     returns alist of component types to be searched in order
-#     using the algorithm we discussed
-#     """
-#
-#     priority_message ="""
-#     Please select what component is most important to you:
-#     """
-#     priorities=[]
-#     # added new parts here for testing, file not complete, not ready yet
-#     comps_to_pick = ["Motherboard", "CPU", "GPU", "RAM", "storage"]
-#
-#     print(priority_message)
-#     while len(comps_to_pick)>0:
-#         print("e: Stop selection.")
-#         counter = 1
-#         selection = 0
-#         for ct in comps_to_pick: # print the option
-#             print(counter,": ", ct)
-#             counter+=1
-#
-#         # get selection until valid number provided
-#         selection = num_selection(len(comps_to_pick))
-#         if selection=='e':
-#             break
-#         priorities.append(comps_to_pick.pop(selection-1))
-#     return priorities
+
